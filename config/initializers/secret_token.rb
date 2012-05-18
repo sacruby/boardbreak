179eaaf6e4a9556e77dbaababcdde86b4b8af001
chaupt <chaupt@webvanta.com>
2012-05-17 20:01:18 -0700

    Raw bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boardbreak::Application.config.secret_token = '34e250c7e33b9a1f914f37a1d1d020f2116b18eb9c971f0028fe11112a057bc75857bfcd0e2a99bb8c2f3beddcc2cd8da30bc490ccac5f1e799c1db54037f85f'
